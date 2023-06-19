6a305b00d3bd37e885d863652c433341430ea19c
MUG030 <ys8267928563@outlook.jp>
2023-06-19 23:27:15 +0900

    UTF-8変換

---
+++ Assets/Scripts/Test.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Test : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        // UTF-8に変更できているか確認するよう
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
